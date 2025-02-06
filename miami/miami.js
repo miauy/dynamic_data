ffc5a4b518acaeb01115d934c3e4c4a55cbcae94
miauy <mau31@miami.edu>
2025-02-06 11:05:02 -0500

    update

--- miami/miami.js
+++ miami/miami.js
@@ -1,13 +1,46 @@
-const express = require('express');
+const express = require('express')
 //initializing express and storing it into variable for easy access
-const app = express();
-//process env from node 
-const port = process.env.port || 3000;
+const app = express()
+
+//set up the template engine
+const handlebars = require('express-handlebars') //additional setup
+app.engine('handlebars', handlebars.engine());
+app.set('view engine', 'handlebars');
+
+
+//process env from node to set port execute: port=8080 node miami
+const port = process.env.port || 3000
+
+
 
 //create some routes
-// app.get('/', (request, response) =>{
+app.get('/', (request, response) =>{
+    response.type('text/html')
+    response.render('home', {title: 'Miami Travel Site'})
+})
 
-// })
+app.get('/about', (request, response) =>{
+    response.type("text/html")
+    response.render("page", {title: 'About Miami'})
+})
+
+app.get('/nightlife', (request, response) =>{
+    response.type("text/html")
+    response.render("page", {title: 'Miami Night Life'})
+})
+app.get('/beaches', (request, response) =>{
+    response.type("text/html")
+    response.render("page", {title: 'Miami Beaches'})
+})
+
+//Query, params and body
+app.get('/search', (request, response) =>{
+    console.log(request)
+    response.type("text/html")
+    response.render("page", {title: 'Search results for: '})
+
+})
+
 
 //error handling goes after actual routes
 //custum 404 error handler
@@ -28,7 +61,8 @@ app.use((error, request, response, next) =>{
 
 //start the server
 app.listen(port, ()=>{
-    console.log(`Express is running on https://localhost:${port};`) //use ` instead of ' when inserting JS into a string
+    console.log(`Express is running on http://localhost:${port};`) //use ` instead of ' when inserting JS into a string
     console.log(`press CTRL-C to terminate`);
 
 })
+
